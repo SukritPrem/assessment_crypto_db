0cefd5d8df5b3493bb5c4e521f09531e777a97ed
Sukrit pipitkul <Surit.pi@ku.th>
2024-06-28 19:03:57 +0700

    delete pool variable not use

--- app/index.js
+++ app/index.js
@@ -1,20 +1,9 @@
 const express = require("express");
-const { Pool } = require("pg");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
-const pool = new Pool({
-  user: "postgres",
-  host: "127.0.0.1",
-  database: "cryptocurrency",
-  password: "postgres",
-  port: 5432,
-});
-
-module.exports = { pool };
 
 app.use("/", require("./router/user"));
 app.use("/admin", require("./router/admin"));
